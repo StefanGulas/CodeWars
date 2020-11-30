cae0077ea34e9601b6eeefb1e0aa274fead1f26f
Stefan <stefan.gulas@gmail.com>
2020-11-30 08:18:47 +0100

    First test is working

--- CodeWars/TopWords.cs
+++ CodeWars/TopWords.cs
@@ -9,18 +9,16 @@ namespace CodeWars
 
     public static List<string> Top3(string s)
     {
-    int top1Counter = 0;
-    int top2Counter = 0;
-    int top3Counter = 0;
-      
+      int top1Counter = 0;
+      int top2Counter = 0;
+      int top3Counter = 0;
+
       if (s == "") return new List<string>() { };
       s = s.ToLower();
       List<string> result = s.Split(' ').ToList();
       result.RemoveAll(item => item == "");
       if (result.Count < 3) return result;
-      var topWords = new Dictionary<string, int>();
-      var top2Words = new Dictionary<string, int>();
-      var top3Words = new Dictionary<string, int>();
+      var topWords = new SortedDictionary<string, int>();
       string actualWord;
       int counter = 0;
       for (int i = 0; i < result.Count; i++)
@@ -34,14 +32,12 @@ namespace CodeWars
         topWords.Add(result[i], counter);
         counter = 0;
       }
-      foreach (KeyValuePair<string, int> item in topWords.OrderBy(key => key.Value))
-      {
-        result.Clear();
-        result.Add(topWords.Keys())
+      result.Clear();
+      var output = topWords.OrderBy(e => e.Value).Select(e => new { e.Key }).ToList();
 
-      }
-
-
+      result.Add(output[output.Count-1].Key.ToString());
+      result.Add(output[output.Count-2].Key.ToString());
+      result.Add(output[output.Count-3].Key.ToString());
 
       return result;
     }
