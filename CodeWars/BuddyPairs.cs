47ab125fbf9e97d36c322bcbfca0200d64cbc523
Stefan <stefan.gulas@gmail.com>
2020-12-20 08:59:02 +0100

    BuddyPairs working but too slow

---
+++ CodeWars/BuddyPairs.cs
@@ -0,0 +1,28 @@
+﻿namespace CodeWars
+{
+  public class BuddyPairs
+  {
+    public static string Buddy(long start, long limit)
+    {
+      long resultSum1 = 0;
+      long resultSum2 = 0;
+      for (int i = (int)start; i <= limit; i++)
+      {
+        for (int j = 1; j <= i / 2; j++)
+        {
+          if (i % j == 0) resultSum1 += j;
+        }
+        long k = resultSum1 - 1;
+        for (int l = 1; l <= k / 2; l++)
+        {
+          if (k % l == 0) resultSum2 += l;
+        }
+        if (resultSum2 - 1 == i && resultSum1 - 1 == k) return "(" + i + " " + k + ")";
+        resultSum1 = 0;
+        resultSum2 = 0;
+      }
+      return "Nothing";
+    }
+  }
+}
+
---
+++ CodeWarsTests/BuddyPairsTest.cs
@@ -0,0 +1,46 @@
+﻿using CodeWars;
+using Xunit;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+  public class BuddyPairsTest
+  {
+    [Fact]
+    public void Test0()
+    {
+
+      string expected = "1,2";
+      string actual = "1,2";
+
+      Assert.Equal(expected, actual);
+    }
+    [Fact]
+    public void BuddyPairsLarge()
+    {
+      static void testing(long start, long limit, string expected)
+      {
+ 
+        Assert.Equal(expected, BuddyPairs.Buddy(start, limit));
+      }
+
+        testing(1071625, 1103735, "(1081184 1331967)");
+        testing(2382, 3679, "Nothing");
+        testing(8983, 13355, "(9504 20735)");
+      }
+    [Fact]
+    public void BuddyPairsSmall()
+    {
+     static void testing(long start, long limit, string expected)
+    {
+
+      Assert.Equal(expected, BuddyPairs.Buddy(start, limit));
+    }
+
+    testing(48, 48, "(48 75)");
+   
+    }
+  }
+}
+  
+
