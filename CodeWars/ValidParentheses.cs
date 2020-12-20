370f478ee20c2210b42b292bddeac9f0f932bcf6
Stefan <stefan.gulas@gmail.com>
2020-12-20 18:54:30 +0100

    ValidParentheses is accepted

---
+++ CodeWars/ValidParentheses.cs
@@ -0,0 +1,25 @@
+﻿namespace CodeWars
+{
+  public class ValidParentheses
+  {
+    public static bool ValidParenthesis(string input)
+    {
+      foreach (var item in input)
+      {
+        if (item.ToString() != "(" && item.ToString() != ")") input = input.Replace(item.ToString(), "");
+      }
+      
+      if (input == "" || input == null) return true;
+      if (!(input[0].ToString() == "(" && input[input.Length - 1].ToString() == ")")) return false; 
+      int count = 0;
+      foreach (var item in input)
+      {
+        if (item.ToString() == "(") count++;
+        else if (item.ToString() == ")") count--;
+        if (count < 0) return false;
+      }
+      if (count == 0) return true;
+      else return false;
+    }
+  }
+}
---
+++ CodeWarsTests/ValidParenthesesTest.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars;
+using Xunit;
+
+namespace CodeWarsTests
+{
+  public class ValidParenthesesTest
+  {
+    [Fact]
+    public void Test0()
+    {
+
+      string expected = "1,2";
+      string actual = "1,2";
+
+      Assert.Equal(expected, actual);
+    }
+    [Fact]
+    public void BuddyPairsLarge()
+    {
+      static void testing(bool expected, string parentheses)
+      {
+
+        Assert.Equal(expected, ValidParentheses.ValidParenthesis(parentheses));
+      }
+
+      testing(true, "()");
+      testing(true, "34543(1081184 1331967)");
+      testing(false, ")((((");
+      testing(false, "()(areztzr)()()()()()((((((()))))(((((((((((()))(()))()())()((");
+      testing(true, "()(areztzr)()()()()()((((((()))))(((((((((((()))(()))()())()(()))))))))))");
+    }
+  }
+}
