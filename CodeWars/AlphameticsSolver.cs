92f07b60f74aa90b0d5d34c0d32fc955f03ac2ef
Stefan <stefan.gulas@gmail.com>
2021-01-01 22:24:50 +0100

    AlphameticsSolver

---
+++ CodeWars/AlphameticsSolver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+  public class AlphameticsSolver
+  {
+    public static string Alphametics(string s)
+    {
+      // your code goes here. you can do it!
+      return "";
+    }
+  }
+}
---
+++ CodeWarsTests/AlphameticsSolverTest.cs
@@ -0,0 +1,31 @@
+﻿using CodeWars;
+using Xunit;
+
+namespace CodeWarsTests
+{
+  public class AlphameticsSolverTest
+  {
+    [Fact]
+    public void Test0()
+    {
+
+      string expected = "1,2";
+      string actual = "1,2";
+
+      Assert.Equal(expected, actual);
+    }
+    [Fact]
+    public void Calculator1()
+    {
+      //      AlphameticsSolver alphameticsSolver = new AlphameticsSolver();
+
+
+      Assert.Equal("", AlphameticsSolver.Alphametics("1-1 +1"));
+      Assert.Equal("", AlphameticsSolver.Alphametics("1-1 +1"));
+      Assert.Equal("", AlphameticsSolver.Alphametics("1-1 +1"));
+      Assert.Equal("", AlphameticsSolver.Alphametics("1-1 +1"));
+      Assert.Equal("", AlphameticsSolver.Alphametics("1-1 +1"));
+
+    }
+  }
+}
