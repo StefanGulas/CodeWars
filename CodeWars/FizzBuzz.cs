a2d40f3a0c1883b37359b6788f657bd040b5ba24
Stefan <stefan.gulas@gmail.com>
2021-01-04 06:24:25 +0100

    FizzBuzz included

---
+++ CodeWars/FizzBuzz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+  public class FizzBuzz
+  {
+    public static string RunTest()
+    {
+      int number = 100;
+      int fizz= 0;
+      int buzz = 0;
+      int fizzBuzz = 0;
+      for (int i = 0; i < number; i++)
+      {
+        if (i % 3 == 0)
+        {
+          if (i % 5 == 0)
+          {
+            fizzBuzz++;
+          }
+          fizz++;
+        }
+        if (i % 5 == 0) buzz++;
+      }
+      return fizz.ToString() + buzz.ToString() + fizzBuzz.ToString();
+    }
+
+
+
+  }
+}
---
+++ CodeWarsTests/FizzBuzzTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CodeWars;
+using Xunit;
+
+namespace CodeWarsTests
+{
+  public class FizzBuzzTest
+  {
+    
+    [Fact]
+    public void Test1()
+    {
+      string actual = FizzBuzz.RunTest();
+      Assert.Equal("122", actual);
+
+    }
+
+  }
+}
